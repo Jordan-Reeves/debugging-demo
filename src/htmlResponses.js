7538f373e5f5bf1508ccf6f1a77b84ff7d29032b
JordanR <jar5927@rit.edu>
2022-09-26 13:39:16 -0400

    Error

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../client/Style.css`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -17,4 +17,5 @@ const getCSS = (request, response) => {
 
 module.exports = {
   getIndex,
+  getCSS,
 };
